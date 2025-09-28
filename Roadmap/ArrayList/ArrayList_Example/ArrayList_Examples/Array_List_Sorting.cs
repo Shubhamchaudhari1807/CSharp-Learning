b4e38bb33b5888a690cef1cfb60d183c3a492ac5
Shubhamchaudhari1807 <131322489+Shubhamchaudhari1807@users.noreply.github.com>
2025-09-29 01:26:04 +0530

    ArrayList Working

---
+++ Roadmap/ArrayList/ArrayList_Example/ArrayList_Examples/Array_List_Sorting.cs
@@ -0,0 +1,46 @@
+﻿// C# program to illustrate
+// sorting of ArrayList
+using System;
+using System.Collections;
+
+public class Array_List_Sorting
+{
+    static public void Main()
+    {
+
+        // Creating ArrayList
+        ArrayList My_array = new ArrayList();
+
+        // Adding elements in the 
+        // My_array ArrayList
+        // This ArrayList contains 
+        // elements of the same types
+        My_array.Add(1);
+        My_array.Add(6);
+        My_array.Add(40);
+        My_array.Add(10);
+        My_array.Add(5);
+        My_array.Add(3);
+        My_array.Add(2);
+        My_array.Add(4);
+
+        // ArrayList before sorting
+        
+
+        foreach (var elements in My_array)
+        {
+            Console.WriteLine(elements);
+        }
+
+        // Sort the elements of the ArrayList
+        // Using sort() method
+        My_array.Sort();
+
+        // ArrayList after sorting
+        
+        foreach (var elements in My_array)
+        {
+            Console.WriteLine(elements);
+        }
+    }
+}
---
+++ Roadmap/ArrayList/ArrayList_Example/ArrayList_Examples/Sample_ArrayList_EX.cs
@@ -0,0 +1,23 @@
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ArrayList_Examples
+//{
+//    public  class Sample_ArrayList_EX
+//    {
+//        public static void Main() 
+//        {
+
+//            ArrayList My_Array = new ArrayList();
+//            My_Array.Add(1);
+//            My_Array.Add(33.33);
+//            My_Array.Add(null);
+//            My_Array.Add('G');
+
+//        }
+//    }
+//}
