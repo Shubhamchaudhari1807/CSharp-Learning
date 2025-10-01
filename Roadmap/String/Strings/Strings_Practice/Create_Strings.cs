5cd564795600aa8ec8f4a344b5536f4e373fbfcb
Shubhamchaudhari1807 <131322489+Shubhamchaudhari1807@users.noreply.github.com>
2025-10-01 21:12:39 +0530

    Strings learning today

---
+++ Roadmap/String/Strings/Strings_Practice/Create_Strings.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings_Practice
+{
+    public class Create_Strings
+    {
+        static void Main()
+        {
+            // Creating String using string literal
+            string str = "Shubhu loves C# ";
+            Console.WriteLine("Method 1: " + str);
+
+
+            // Creating String using concatenation
+            string str2 = str + " And learn Fullstack Development";
+            Console.WriteLine(str2);
+
+            // Creating a string using a constructor
+            char[] create_Strings_ways = { 'S', 'H', 'U', 'B', 'H', 'A', 'M' };
+            string full_ctrs_String = new string(create_Strings_ways);
+            Console.WriteLine(full_ctrs_String);
+
+            // Creating a string using a property or a method
+            String s = "Geeks For Geeks";
+
+            // Index of 
+            int start = s.IndexOf(" ") + 1;
+            int end = s.IndexOf(" ", start) - start;
+            string str4 = s.Substring(start, end);
+            Console.WriteLine("Method 4: " + str4);
+
+            // Creating a string using formatting
+            int i = 1;
+            int j = 2;
+            int sum = i + j;
+            String str5 = string.Format("Addition of {0} with {1} is {2}"
+                                      , i, j, sum);
+            Console.WriteLine("Method 5: " + str5);
+
+
+        }
+    }
+}
---
+++ Roadmap/String/Strings/Strings_Practice/Strings_Example_First.cs
@@ -0,0 +1,37 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Strings_Practice
+//{
+//    public class Strings_Example_First
+//    {
+
+
+//        public static void Main(string[] args)
+//        {
+//            // declaraing the  string name using  "System.String" Class
+//            System.String Name;
+
+//            // Initialization Of  string
+//            Name = "Shubham Bhai";
+
+//            string id;
+//            id = "33";
+
+//            string mrk;
+//            mrk = "97";
+
+//            // Declaration and initialization of the string in a single line
+//            string rank = "12";
+
+//            Console.WriteLine($"Name : {Name}, Id : {id}, Marks : {mrk}, Rank : {rank}");
+
+
+//            Console.WriteLine("Name : {0}, Id : {1}, Marks : {2}, Rank : {3}",Name,id,mrk,rank);
+
+//        }
+//    }
+//}
